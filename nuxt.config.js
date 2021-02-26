44ef4e3a8d7d603df1b063422d19d6a2ec4485ab
kikuchimasayuki <masakangs1@gmail.com>
2021-02-26 15:00:33 +0900

    Add index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  css: ['ress', '~/assets/css/foundation.scss'],
+
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
